851431553622a707d26389c54ab16d0a4772e904
basheerkattan <basheerkattan2060@gmail.com>
2023-01-10 13:59:15 +0100

    to pull the new branches

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import classes from './Home.module.css'
+import React from 'react';
+import classes from './Home.module.css';
 
 export default function Home() {
-
-  const  SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET; 
-  const  SPOTIFY_CLIENT_ID = process.env.REACT_APP_CLIENT_ID; 
+  const SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+  const SPOTIFY_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
   fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
-    body: 'grant_type=client_credentials&client_id=' + SPOTIFY_CLIENT_ID.toString('base64') + '&client_secret=' + SPOTIFY_CLIENT_SECRET.toString('base64') ,
+    body:
+      'grant_type=client_credentials&client_id=' +
+      SPOTIFY_CLIENT_ID.toString('base64') +
+      '&client_secret=' +
+      SPOTIFY_CLIENT_SECRET.toString('base64'),
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
-.then(r => r.json())
-.then(r => {
-    console.log(r.access_token)
-})
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then(r => r.json())
+    .then(r => {
+      console.log(r.access_token);
+    });
 
-
-
-  return (
-    <div className={classes.main}>
-      
-    </div>
-  )
+  return <div className={classes.main}></div>;
 }
