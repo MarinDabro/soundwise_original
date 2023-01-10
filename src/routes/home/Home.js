58148fda0e628126dafcd5041051a07895c664c9
basheerkattan <basheerkattan2060@gmail.com>
2023-01-10 10:53:14 +0100

    solved the api problem and added the baseCode64

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -2,7 +2,26 @@ import React from 'react'
 import classes from './Home.module.css'
 
 export default function Home() {
+
+  const  SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET; 
+  const  SPOTIFY_CLIENT_ID = process.env.REACT_APP_CLIENT_ID; 
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials&client_id=' + SPOTIFY_CLIENT_ID.toString('base64') + '&client_secret=' + SPOTIFY_CLIENT_SECRET.toString('base64') ,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+.then(r => r.json())
+.then(r => {
+    console.log(r.access_token)
+})
+
+
+
   return (
-    <div className={classes.main}>Home</div>
+    <div className={classes.main}>
+      
+    </div>
   )
 }
