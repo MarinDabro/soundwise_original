ccd4546a543141233c0f8ba8f5a83dca32e2b1df
basheerkattan <basheerkattan2060@gmail.com>
2023-02-13 14:55:09 +0100

    working on the player

--- src/routes/newRelease/NewRelease.js
+++ src/routes/newRelease/NewRelease.js
@@ -2,16 +2,20 @@ import React, { useEffect, useContext } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import DisplayContext from "../../context/DisplayContext.js";
 import MainContext from "../../context/MainContext";
+import PlayerContext from "../../context/PlayerContext.js";
 import style from "../MusicBox.module.css";
+import classes from '../../components/player/PlayerButton.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faPause,
 } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 //import Bouncer from "../../functions/bouncer.js";
 
 export default function NewRelease(props) {
-  const [{ newRelease, token, hashToken }, DISPATCH] = useContext(MainContext);
+  const [{ playerState }, playerDispatch] = useContext(PlayerContext);
+  const [{ newRelease, token, hashToken}, DISPATCH] = useContext(MainContext);
   const [{ navReminder }, dispatch] = useContext(DisplayContext);
   const navigate = useNavigate();
   useEffect(() => {
@@ -39,6 +43,38 @@ export default function NewRelease(props) {
     getNewRelease();
   }, []);
 
+  const changeState = async () => {
+    const state = playerState ? "pause" : "play";
+    if (hashToken) {
+      /* const deviceRes = await axios.get(
+        "https://api.spotify.com/v1/me/player/devices ",
+        {
+          headers: headersParam,
+        }
+      );
+      const deviceId = deviceRes.data.devices[0].id; */
+      /* /volume?volume_percent=55&device_id="${deviceId}" 
+      deviceId &&*/
+      await axios.put(
+        `https://api.spotify.com/v1/me/player/${state}`,
+
+        {},
+        {
+          headers: headersParam,
+        }
+      );
+    }
+    playerDispatch({
+      type: "SET_PLAYER_STATE",
+      playerState: !playerState,
+    });
+  };
+  const headersParam = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + hashToken,
+  };
+
+
   return (
     <div className={style.main}>
       <h2>New Releases</h2>
@@ -55,28 +91,28 @@ export default function NewRelease(props) {
                 >
                   <div className={style.albumImage}>
                     <img src={album.images[1].url} alt="/artist_image" />
+                    <div className={classes["play-button"]}>
+        {playerState ? (
+          <FontAwesomeIcon
+            className={classes["player-icon"]}
+            icon={faPause}
+            onClick={changeState}
+          />
+        ) : (
+          <FontAwesomeIcon
+            className={classes["player-icon"]}
+            icon={faPlay}
+            onClick={changeState}
+          />
+        )}
+      </div>
                   </div>
                   <div className={style.albumName}>{album.name}</div>
                   <div className={style.artistName}>
                     BY: {album.artists[0].name}
                   </div>
                 </NavLink>
-                <button
-                  onClick={() => {
-                    if (!hashToken) {
-                      dispatch({
-                        type: "SET_SONG_REMINDER",
-                        songReminder: true,
-                      });
-                      dispatch({
-                        type: "SET_SONG_INFO",
-                        songInfo: album,
-                      });
-                    }
-                  }}
-                >
-                  click
-                </button>
+              
               </div>
             );
           })}
