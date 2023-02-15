f496bad11007e0c989b971eb300c8dab9e1283ca
basheerkattan <basheerkattan2060@gmail.com>
2023-02-15 11:28:35 +0100

    trying to deploy

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -20,7 +20,7 @@ export default function Home(props) {
   useEffect(() => {
     fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
-      body: `grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}`,
+      body: `grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}&redirect_uri=https://soundwiseoriginal.onrender.com`,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
